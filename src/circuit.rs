4b8714eaa79b988c6d6e2f88ac2445d8b0d5a6d3
RequiemOfSouls <RequiemOfSouls@zklink.org>
2021-10-27 22:52:48 +0800

    optimized code.

--- src/circuit.rs
+++ src/circuit.rs
@@ -93,18 +93,16 @@ impl<F: PrimeField> ConstraintSynthesizer<F> for SecretStringCircuit<F> {
         }
 
         // calculate secret hash
-        assert!(256 >= F::Params::CAPACITY, "select correct F bits length");
-        let padding = vec![Boolean::constant(false); 256 - F::Params::CAPACITY as usize];
         let mut secret_commitment_bits =
             sha256(cs.ns(|| "calculate secret string hash"), &secret_bits)?;
         secret_commitment_bits.reverse();
-        secret_commitment_bits[F::Params::CAPACITY as usize..].copy_from_slice(&padding);
+        secret_commitment_bits[F::Params::CAPACITY as usize..].fill(Boolean::constant(false));
 
         // calculate message hash
         let mut message_commitment_bits =
             sha256(cs.ns(|| "calc signed message hash"), &signed_message_bits)?;
         message_commitment_bits.reverse();
-        message_commitment_bits[F::Params::CAPACITY as usize..].copy_from_slice(&padding);
+        message_commitment_bits[F::Params::CAPACITY as usize..].fill(Boolean::constant(false));
 
         // Check whether the secret hash is correctly calculated
         let final_secret_hash = pack_bits_to_element(
