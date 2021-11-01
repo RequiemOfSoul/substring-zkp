6784c37af831b31e7a4ae6b1d0ebed47e5ed725e
RequiemOfSouls <RequiemOfSouls@zklink.org>
2021-11-01 15:58:36 +0800

    modified substring_zkp to crate lib.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,13 @@
-#![allow(non_snake_case)]
-#![allow(dead_code)]
-mod circuit;
-mod circuit_extend;
-mod params;
-mod test_constraint_system;
-mod utils;
-mod witness;
-
 use ark_serialize::*;
 use ark_std::test_rng;
 use std::time::Instant;
 
 // use ark_bls12_381::{Bls12_381 as E, Fr};
-use crate::utils::generate_circuit_instance;
 use ark_bn254::Bn254 as E;
 use ckb_groth16::{
     create_random_proof, generate_random_parameters, verifier::prepare_verifying_key, verify_proof,
 };
+use substring_zkp::{params, utils::generate_circuit_instance};
 
 fn main() {
     let rng = &mut test_rng();
@@ -24,9 +15,8 @@ fn main() {
     let padding = "0"; // must be single char, or else fill it to MAX_HASH_PREIMAGE_LENGTH
     let secret = "christian.schneider@androidloves.me";
     let mut padding_message = "from:Christian Schneider Christian Schneider Christian Schneider <christian.schneider@androidloves.me>\r\nsubject:this is a test mail\r\ndate:Sat, 14 Mar 2020 21:48:57 +0100\r\nmessage-id:<4c2828df-2dae-74ff-2fa7-e6ac36100341@androidloves.me>\r\nto:mail@kmille.wtf\r\ncontent-type:text/plain; charset=utf-8; format=flowed\r\ncontent-transfer-encoding:7bit\r\ndkim-signature:v=1; a=rsa-sha256; c=relaxed/relaxed; d=androidloves.me; s=2019022801; t=1584218937; h=from:from:reply-to:subject:subject:date:date:message-id:message-id: to:to:cc:content-type:content-type: content-transfer-encoding:content-transfer-encoding; bh=aeLbTnlUQQv2UFEWKHeiL5Q0NjOwj4ktNSInk8rN/P0=; b=".to_string();
-    padding_message.push_str(
-        &*padding.repeat(crate::params::MAX_HASH_PREIMAGE_LENGTH - padding_message.len()),
-    );
+    padding_message
+        .push_str(&*padding.repeat(params::MAX_HASH_PREIMAGE_LENGTH - padding_message.len()));
 
     let (c, public_input) = generate_circuit_instance(secret.to_string(), padding_message);
 
